31a6bb0113a9cee8f1db75f2e142dcabe90421b6
JoshDev <joshobrien4@hotmail.com>
2016-03-09 15:50:15 +0000

    Created search template, edited settings. created css file.

--- ehealth/settings.py
+++ ehealth/settings.py
@@ -12,12 +12,14 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_PATH =os.path.join(BASE_DIR, 'templates')
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
-#Our template path:
+#Our paths
 TEMPLATE_DIRS = (
     TEMPLATE_PATH,
 
     )
+    
 
 
 # Quick-start development settings - unsuitable for production
@@ -89,3 +91,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS=(
+    STATIC_PATH,
+)
