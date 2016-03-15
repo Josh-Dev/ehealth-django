5ce693cb8cefcab6f0a20c2dec8f363fc71e5c1e
JackyL <2137902l@BO715-11-04.ad.dcs.gla.ac.uk>
2016-03-15 16:26:24 +0000

    templates working and updated

--- ehealth_project/urls.py
+++ ehealth_project/urls.py
@@ -2,13 +2,12 @@ from django.conf.urls import patterns, url
 from ehealth_project import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.search, name='search'),
-    #url(r'^about/$', views.about, name='about'),
+    url(r'^$', views.basic_search, name='basic_search'),
+    url(r'^about/$', views.about, name='about'),
     url(r'^register/$', views.register, name='register'),
     #url(r'^login/$', views.user_login, name='login'),
     #url(r'^user_finder', views.user_finder, name='user_finder'),
     url(r'^register/$', views.register, name='register'),
-    url(r'^advanced',
-        views.advanced_search,
-        name='advanced_search'),
+    url(r'^advanced',views.advanced_search,name='advanced_search'),
+	url(r'^how-do-we-work',views.how,name='how'),
 )
