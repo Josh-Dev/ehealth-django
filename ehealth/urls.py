e69d9a51ef1241347fe447946f5fef2e6b3dcef9
JackyL <2137902l@BO715-11-04.ad.dcs.gla.ac.uk>
2016-03-15 15:17:01 +0000

    updated urls

--- ehealth/urls.py
+++ ehealth/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
     #Urls
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^ehealth/', include('ehealth-project.urls')),
+    url(r'^ehealth/', include('ehealth_project.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls'))
 )
--- ehealth_project/views.py
+++ ehealth_project/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from ehealth_project.models import UserProfile
+#from ehealth_project.forms import UserFinderForm
+
+def register(request):
+    render(request,'ehealth_projects/base.html', {})
+
+def advanced_search(request):
+    render(request,'ehealth_projects/base.html', {})
+
+def search(request):
+    context_dict = {}
+    render(request, 'ehealth_project/search.html',context_dict)
+
