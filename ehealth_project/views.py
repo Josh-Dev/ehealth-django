bbfee4b4d591784ebb84dcf05798aa6e0617f2eb
Bad Mo <badmo@fo.com>
2016-03-15 15:37:15 +0000

    Resolved conflicts

--- ehealth_project/views.py
+++ ehealth_project/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 
-from ehealth_project.models import UserProfile
-#from ehealth_project.forms import UserFinderForm
 
 def register(request):
-    return render(request,'ehealth_projects/base.html', {})
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        if user_form.is_valid()
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+        else:
+            print user_form.errors
+    else:
+        user_form = UserForm()
+    return render(request,
+            'ehealth/register.html',
+            {'user_form': user_form, 'profile_form': profile_form, 'registered': registered} )
 
 def advanced_search(request):
     return render(request,'ehealth_projects/base.html', {})
