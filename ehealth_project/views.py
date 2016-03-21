b4aaac43a2767fbb27ae47f5e8335e7771ecfc1b
Bad Mo <badmo@fo.com>
2016-03-21 12:35:17 +0000

    Login update

--- ehealth_project/views.py
+++ ehealth_project/views.py
@@ -198,4 +198,32 @@ from django.contrib.auth import logout
 
 def user_logout(request):
     logout(request)
-    return HttpResponseRedirect('/rango/')
+    return HttpResponseRedirect('/ehealth/')
+def user_login(request):
+
+    
+    if request.method == 'POST':
+        
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        
+        user = authenticate(username=username, password=password)
+
+        if user:
+            
+            if user.is_active:
+               
+                login(request, user)
+                return HttpResponseRedirect('/ehealth/')
+            else:
+               
+                return HttpResponse("Your ehealth account is disabled.")
+        else:
+            
+            print "Invalid login details: {0}, {1}".format(username, password)
+            return HttpResponse("Invalid login details supplied.")
+
+    else:
+       
+        return render(request, 'ehealth_project/login.html', {})
