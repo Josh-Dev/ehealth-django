757f928951e870f518addf3f0d996aff249d2475
Billodie3000 <jack.longie@live.com>
2016-03-22 22:02:29 +0800

    tidying

--- Workspace/Webapp/Group Project/ehealth-django/Workspace/Webapp/Group Project/ehealth-django/README.md
+++
@@ -1,2 +0,0 @@
-# ehealth-django
-eHealth application. 
--- Workspace/Webapp/Group Project/ehealth-django/Workspace/Webapp/Group Project/ehealth-django/ehealth_project/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from ehealth_project.models import Folder, UserProfile, Page
-
-# Register your models here.
-admin.site.register(Folder)
-admin.site.register(Page)
-admin.site.register(UserProfile)
